993f0e64979eae55ae6bef833ecb2958b7f67994
Suraj Thapa <surajthapa@Surajs-MacBook-Pro.local>
2024-09-16 13:44:42 +0545

    AUTHENTICATION - [x] Authentication graphql api - [x] User seeder

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../shared/prisma/prisma.service';
 import { JwtAuthService } from '../shared/jwt/jwt.service';
+import * as E from 'fp-ts/Either';
+import {
+  INTERNAL_SERVER_ERROR,
+  INVALID_CREDENTIALS,
+} from '../shared/constants/status.codes';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,14 +15,31 @@ export class AuthService {
     private readonly prisma: PrismaService,
   ) {}
 
-  async login(username: string): Promise<any> {
-    console.log(username);
-    const user = await this.prisma.user.findFirst();
-    const token = await this.jwt.generateToken(user);
-    console.log({ user, token });
-    return {
-      user,
-      token,
-    };
+  async login(email: string, password: string) {
+    try {
+      const user = await this.prisma.user.findFirst({
+        where: {
+          email,
+        },
+      });
+
+      if (!user || !(await bcrypt.compare(password, user.password))) {
+        return E.left({
+          message: 'Invalid Credentials',
+          code: INVALID_CREDENTIALS,
+        });
+      }
+
+      const token = await this.jwt.generateToken(user);
+      return E.right({
+        user,
+        token,
+      });
+    } catch (exception) {
+      return E.left({
+        message: 'Internal Server error',
+        code: INTERNAL_SERVER_ERROR,
+      });
+    }
   }
 }
